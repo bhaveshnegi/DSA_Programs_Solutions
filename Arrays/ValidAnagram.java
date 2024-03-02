b422e06b8efcea2d48a069a0c4b7e9595bccaa65
bhaveshnegi <bhaveshnegi98@gmail.com>
2024-03-02 17:33:18 +0530

    Valid Anagram

--- Arrays/ValidAnagram.java
+++ Arrays/ValidAnagram.java
@@ -1,11 +1,17 @@
-class Solution {
-    public boolean isAnagram(String s, String t) {
-        char[] sChars = s.toCharArray();
-        char[] tChars = t.toCharArray();
-        
-        Arrays.sort(sChars);
-        Arrays.sort(tChars);
-        
-        return Arrays.equals(sChars, tChars);
+package DSA_Programs_Solutions.Arrays;
+
+import java.util.Arrays;
+
+public class ValidAnagram {
+    class Solution {
+        public boolean isAnagram(String s, String t) {
+            char[] sChars = s.toCharArray();
+            char[] tChars = t.toCharArray();
+    
+            Arrays.sort(sChars);
+            Arrays.sort(tChars);
+    
+            return Arrays.equals(sChars, tChars);
+        }
     }
 }
