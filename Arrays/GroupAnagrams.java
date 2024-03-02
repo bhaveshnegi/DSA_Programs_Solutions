baa7db6da8b79d5b6d820c6adcfb4c01d1dc8e81
bhaveshnegi <bhaveshnegi98@gmail.com>
2024-03-02 17:49:41 +0530

    Group Anagrams

---
+++ Arrays/GroupAnagrams.java
@@ -0,0 +1,28 @@
+package DSA_Programs_Solutions.Arrays;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class GroupAnagrams {
+    class Solution {
+        public List<List<String>> groupAnagrams(String[] strs) {
+            Map<String, List<String>> map = new HashMap<>();
+            
+            for (String word : strs) {
+                char[] chars = word.toCharArray();
+                Arrays.sort(chars);
+                String sortedWord = new String(chars);
+                
+                if (!map.containsKey(sortedWord)) {
+                    map.put(sortedWord, new ArrayList<>());
+                }
+                
+                map.get(sortedWord).add(word);
+            }
+            
+            return new ArrayList<>(map.values());
+        }
+    }
+}
