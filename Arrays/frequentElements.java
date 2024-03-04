2e274224b131ba44545773ff8d8a81c0f42f10fb
bhaveshnegi <bhaveshnegi98@gmail.com>
2024-03-04 14:51:36 +0530

    Top K Frequent Elements

---
+++ Arrays/frequentElements.java
@@ -0,0 +1,29 @@
+package DSA_Programs_Solutions.Arrays;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.PriorityQueue;
+
+public class frequentElements {
+    class Solution {
+        public int[] topKFrequent(int[] nums, int k) {
+            Map<Integer, Integer> map = new HashMap<>();
+            
+            for(int i : nums){
+                map.put(i, map.getOrDefault(i, 0) + 1);
+            }
+            
+            PriorityQueue<Integer> maxHeap = new PriorityQueue<>((a,b) -> map.get(b) - map.get(a));
+            
+            for(int key : map.keySet()){
+                maxHeap.add(key);
+            }
+            
+            int res[] = new int[k];
+            for(int i = 0; i < k; i++){
+                res[i] = maxHeap.poll();
+            }
+            return res;
+        }
+    }
+}
